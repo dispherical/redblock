e62deebe1b15ff065701a7c793c288e05acb5643
dispherical <62346025+dispherical@users.noreply.github.com>
2025-10-18 22:36:24 -0400

    Update reloadipset.sh

--- reloadipset.sh
+++ reloadipset.sh
@@ -12,7 +12,7 @@ echo "[Redblock] Building ipset restore file..."
   echo "create blocked4 hash:net family inet hashsize 65536 maxelem 10000000"
   awk '/\./{print "add -exist blocked4", $0}' clean-list.txt
   echo "create blocked6 hash:net family inet6 hashsize 65536 maxelem 10000000"
-  awk -F: '{if (NF>1) print "add -exist blocked6", $0}' clean-list.txt
+  awk -F: '{if ($0 ~ /^([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{0,4}(\/[0-9]{1,3})?$/) print "add -exist blocked6", $0}' clean-list.txt
 } > ipset-restore.txt
 
 sudo ipset destroy blocked4 2>/dev/null || true
